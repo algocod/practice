53176fdbc0655d282400c6db0cd37d66fb08105f
hemant <hemant@HemAnj.home>
2018-05-15 07:03:27 -0400

    Rotate matrix and MatrixExpo

--- Java8Testing/src/datastructures/BinarySearchTree.java
+++ Java8Testing/src/datastructures/BinarySearchTree.java
@@ -1,10 +1,15 @@
 package datastructures;
 
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Addition of elements to a tree. >> Key is setting to the Java object in method like node.setRIghtNode(new BstNode(8))
 (a) Inorder (Left, Root, Right) : 4 2 5 1 3
 (b) Preorder (Root, Left, Right) : 1 2 4 5 3
 (c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
+// Other problems include , Printing a tree wihtout recursion and Reversing a stack without any Data strucutres.
  * @author hemant
  *
  */
@@ -24,8 +29,20 @@ public class BinarySearchTree {
 		String sysOut = print(mainRoot);
 		System.out.println(sysOut);
 
-		boolean gotit = find(mainRoot, 4);
-		System.out.println(gotit);
+		Stack<BSTNode> stck = new Stack<>();	
+		inOrderWithoutRecursion(mainRoot, stck);
+		
+		Stack<Integer> stack = new Stack();
+		int i = 1;
+		while (i<6)
+		{
+			stack.push(i);
+			i++;
+		}
+		reverseStack(stack);
+		//
+		//boolean gotit = find(mainRoot, 4);
+		//System.out.println(gotit);
 	}
 	
 	public static void addToTree(BSTNode rt , Integer val)
@@ -62,23 +79,54 @@ public class BinarySearchTree {
 		}
 	}
 	
-	public static int getElementAtIndex(BSTNode node, int index, int starting)
+	// reverse a stack without using any other data strucutres
+	public static void reverseStack(Stack<Integer> stack)
 	{
-		//try entering this into a stack and then pop it out.
-		/*
-		if(node!=null && starting==index)
-		{
-			return node.getValue();
-		}
-		else if (node!=null)
-		{
-			starting++;
-			getElementAtIndex(node.getLeftNode(), index, starting) 
-			
-		}
-		*/
+		if(stack.isEmpty())
+			return ;
 		
-		return 0;
+		int tmp = stack.pop();
+		reverseStack(stack);
+		insertNumFirst(stack, tmp);
+		
+	}
+	
+	public static void insertNumFirst(Stack<Integer> stck, Integer i)
+	{
+		if(stck.isEmpty())
+		{
+			stck.push(i);
+			return;
+		}
+		
+		Integer tmp = stck.pop();
+		insertNumFirst(stck, i);
+		stck.push(tmp);
+	}
+	
+	// Move to the very left node then one by one move to the right nodes 
+	// At each right node , add any exisitng left nodes
+	public static void inOrderWithoutRecursion(BSTNode node, Stack<BSTNode> stck)
+	{
+		//create a queue first
+		addToStackToLeft(stck, node);
+		
+		while(!stck.isEmpty())
+		{
+			node = stck.pop();
+			System.out.println(node.getValue());
+			
+			addToStackToLeft(stck, node.getRightNode());
+		}
+	}
+	
+	public static void addToStackToLeft(Stack<BSTNode> stack, BSTNode node)
+	{
+		while(node!=null)
+		{
+			stack.push(node);
+			node = node.getLeftNode();
+		}
 	}
 	
 	public static boolean find(BSTNode node, Integer val)
--- Java8Testing/src/interviewQs/StepsProblem.java
+++ Java8Testing/src/interviewQs/StepsProblem.java
@@ -1,18 +1,59 @@
 package interviewQs;
 
+/**
+ * 5
+21
+35
+24
+36
+30
+
+223317
+1132436852
+1389537
+2082876103
+53798080
+
+ * @author hemant
+ *
+ */
+		
 public class StepsProblem 
 {
 	
 public static void main(String[] args)
 {
+
 	System.out.println(countSteps(3));
-	System.out.println(countSteps(4));
-	System.out.println(countSteps(5));
-	System.out.println(countSteps(10));
+	System.out.println(calcWays(4));
+	System.out.println(calcWays(5));
+	System.out.println(calcWays(10));
+
+	
+	System.out.println(calcWays(21));
+	System.out.println(calcWays(35));
+	System.out.println(calcWays(24));
+	System.out.println(calcWays(36));
+	System.out.println(calcWays(30));
+	
+
 }
 	
 	
-	
+public static int calcWays(int n) {
+    if (n < 1) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (n == 3) return 4;
+    
+    int[] ways = new int[] {1,2,4};
+    
+    for (int i = 4; i < n; i++) {
+        int idx = (i - 1) % 3;
+        ways[idx] = ways[0] + ways[1] + ways[2];
+    }
+    return ways[0] + ways[1] + ways[2];
+}	
 
 public static int countSteps(int n)
 {
