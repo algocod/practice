1db095999592f79aeda475dddfb625cac0fff634
hemant <hemant@HemAnj.home>
2018-06-07 05:49:59 -0400

    Graph and some java runtime examples

---
+++ Java8Testing/src/algorithms/graph/BFSInit.java
@@ -0,0 +1,67 @@
+package algorithms.graph;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class BFSInit 
+{
+	
+	public static void main (String[] args)
+	{
+		
+		Graph graph = new Graph(5);
+		 GraphAdjList.addEdge(graph, 0, 1);
+		 GraphAdjList.addEdge(graph, 0, 4);
+		 GraphAdjList.addEdge(graph, 1, 2);
+		 GraphAdjList.addEdge(graph, 1, 3);
+		 GraphAdjList.addEdge(graph, 1, 4);
+		 GraphAdjList.addEdge(graph, 2, 3);
+		 GraphAdjList.addEdge(graph, 3, 4);
+	     
+		 printBFS(graph,1);
+		 
+/*
+		Graph g = new Graph(4);
+		 
+		GraphAdjList.addEdge(g,0, 1);
+		GraphAdjList.addEdge(g,0, 2);
+		GraphAdjList.addEdge(g,1, 2);
+		GraphAdjList.addEdge(g,2, 0);
+		GraphAdjList.addEdge(g,2, 3);
+		GraphAdjList.addEdge(g,3, 3);
+		
+		printBFS(g, 2);
+		*/
+	}
+	
+	public static void printBFS(Graph g, int startNode)
+	{
+		Queue<Integer> q = new LinkedBlockingQueue<Integer>(g.V);
+		
+		LinkedList<Integer>[] adj = g.adjListArray;
+		int[] visited = new int[5];
+		
+		// lets start with node 2, which will position 1 in the adjArray list
+		q.add(startNode);
+		visited[startNode] = 1;
+		while(!q.isEmpty())
+		{
+			int node = q.poll();
+			System.out.println(node);
+			Iterator<Integer> l = adj[node].listIterator();
+			while(l.hasNext())
+			{
+				Integer n = l.next();
+				if(visited[n]!=1)
+				{
+					q.add(n);
+					visited[n]=1;
+				}
+					
+			}
+		}
+	}
+
+}
--- Java8Testing/src/javapractical/RunTesting.java
+++ Java8Testing/src/javapractical/RunTesting.java
@@ -1,5 +1,7 @@
 package javapractical;
 
+import java.util.HashMap;
+
 public class RunTesting {
 
 	public static void main(String[] args) {
@@ -12,6 +14,20 @@ public class RunTesting {
 		tfsI.workHard();
 		tfsI2.workHard();
 		
+		int a =1 <<2;
+		//int b = a++;
+		//int c = ++b;
+		System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
 	}
 
+	public int test(int a , int b) throws Exception
+	{
+		
+		if (b==0)
+			throw new RuntimeException("zero");
+		return (a>b)?a:b;
+	}
+	
 }
