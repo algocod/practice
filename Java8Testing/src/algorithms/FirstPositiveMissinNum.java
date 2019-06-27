a105f8693d4bcd8a0194f966d9cc0cafe44c258e
hemant <hemant@HemAnj.home>
2019-06-27 06:23:14 -0400

    With O(n) space its straight forward

---
+++ Java8Testing/src/algorithms/FirstPositiveMissinNum.java
@@ -0,0 +1,36 @@
+package algorithms;
+
+import java.util.HashSet;
+
+public class FirstPositiveMissinNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int firstMissingPositive(int[] nums) 
+    {
+        int min = 0;
+        int max = 0;;
+        HashSet<Integer> hst = new HashSet<>();
+        for(int a : nums)
+        {
+            if(a>0)
+            {
+                max = Math.max(a,max);
+                hst.add(a);
+            }
+            
+        }
+        
+        int find = 1;
+        while(hst.contains(find))
+        {
+            find++;
+        }
+        
+        return find;
+    }
+	
+}
